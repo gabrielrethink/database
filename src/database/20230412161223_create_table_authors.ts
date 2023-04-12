80827dd704dfff868ddf4f53b066493eb804b608
Gabriel Gomes <gabriel.gomes@rethink.dev>
2023-04-12 13:52:00 -0300

    making authors table

--- src/database/20230412161223_create_table_authors.ts
+++ src/database/20230412161223_create_table_authors.ts
@@ -1,13 +1,13 @@
 import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable("books", function (table) {
+  await knex.schema.createTable("authors", function (table) {
     table.increments();
     table.string("name").notNullable();
-    table.string("author").notNullable();
+    table.string("bestSeller");
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable("books");
+  await knex.schema.dropTable("authors");
 }
